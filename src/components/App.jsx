465aff823f6278d82e770e3e98c10e478d172be0
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-06-23 20:08:16 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,9 @@
+import { Card } from './Profile/profile';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template 
-    </div>
-  );
+  return <Card />;
 };
 
+// ReactDOM.createRoot(document.getElementById("root")).render(<Card/>)
 
 // Компоненты всех заданий рендерятся на одной странице, внутри общего контейнера - корневого компонента <App>.
