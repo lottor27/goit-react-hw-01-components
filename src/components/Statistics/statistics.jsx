d5ce7801df9c8625cbbd81d85b0b64829616b9aa
lottor27 <120725846+lottor27@users.noreply.github.com>
2023-06-29 20:37:07 +0300

    124545

--- src/components/Statistics/statistics.jsx
+++ src/components/Statistics/statistics.jsx
@@ -1,5 +1,11 @@
 import css from './statistics.module.css';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
 export default function Statistics({ stats, title }) {
   return (
     <section className={css.statistics}>
@@ -8,7 +14,12 @@ export default function Statistics({ stats, title }) {
 
         <ul className={css.statList}>
           {stats.map(stat => (
-            <li className={css.item} key={stat.id} id={stat.id}>
+            <li
+              className={css.item}
+              key={stat.id}
+              id={stat.id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
               <span className={css.label}>{stat.label}</span>
               <span className={css.percentage}>{stat.percentage}%</span>
             </li>
@@ -18,3 +29,4 @@ export default function Statistics({ stats, title }) {
     </section>
   );
 }
+
